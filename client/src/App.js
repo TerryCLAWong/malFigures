a3c666f63fadf4e9532dfada7515a8c74f898fbe
TerryCLAWong <anselm.wong@mail.utoronto.ca>
2021-01-16 20:29:40 -0500

    added simple banner component

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 //Components
 import Content from './components/content/content'
 import Navigation from './components/navigation/navigation'
+import Banner from './components/banner/banner'
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
         <div className="App">
+          <Banner/>
           <Navigation/>
           <Content/>
         </div>
---
+++ client/src/components/banner/banner.js
@@ -0,0 +1,11 @@
+import React, { Component }  from 'react';
+
+class Banner extends Component {
+    render () {
+        return (
+            <h1>THIS IS A BANNER</h1>
+        )
+    }
+}
+
+export default Banner
